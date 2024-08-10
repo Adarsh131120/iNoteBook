854ecca646783fc7febef18f909f97e51e515799
Adarsh Tiwari personal <adarshtiwari131120@gmail.com>
2024-08-10 15:09:28 +0530

    main

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // const mongoURI = "mongodb://localhost:27017/inotebook"; 
+// hello 
 const mongoURI = process.env.DB_URL; 
 
 const connectToMongo = async () => {
